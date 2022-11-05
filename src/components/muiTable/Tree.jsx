14cde13c865b4d55084c12bd01936afb6b82cbc7
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-11-05 14:38:22 +0530

    Update Tree.jsx

--- src/components/muiTable/Tree.jsx
+++ src/components/muiTable/Tree.jsx
@@ -6,26 +6,27 @@ const Tree = () => {
   return (
     <TreeView
         aria-label="multi-select"
-        defaultCollapseIcon={<RemoveCircleOutline />}
-        defaultExpandIcon={<AddCircleOutline />}
+        defaultCollapseIcon={<RemoveCircleOutline style={{ fontSize: '26px', color: '#1775e5' }}/>}
+        defaultExpandIcon={<AddCircleOutline style={{ fontSize:'26px', color: '#1775e5' }}/>}
         multiSelect
+        className='tree'
         sx={{ height: 300, flexGrow: 1, maxWidth: 500, overflowY: 'auto' }}
     >
-        <TreeItem nodeId="1" label="Employee1">
+        <TreeItem nodeId="1" label="Employee1" className='node' style={{ fontSize: '32px' }}>
             <TreeItem nodeId="2" label="Employee2" />
-            <TreeItem nodeId="2" label="Employee3">
-                <TreeItem nodeId="3" label="Employee5" />
-                <TreeItem nodeId="3" label="Employee6" />
+            <TreeItem nodeId="2" label="Employee3" className='node'>
+                <TreeItem nodeId="4" label="Employee5" />
+                <TreeItem nodeId="4" label="Employee6" />
             </TreeItem>
-            <TreeItem nodeId="2" label="Employee4" />
-            <TreeItem nodeId="2" label="Employee7">
-                <TreeItem nodeId="4" label="Employee8" />
-                <TreeItem nodeId="4" label="Employee9">
-                    <TreeItem nodeId="5" label="Employee10" />
-                    <TreeItem nodeId="5" label="Employee11" />
+            <TreeItem nodeId="3" label="Employee4" />
+            <TreeItem nodeId="3" label="Employee7" className='node'>
+                <TreeItem nodeId="5" label="Employee8" />
+                <TreeItem nodeId="6" label="Employee9" className='node'>
+                    <TreeItem nodeId="7" label="Employee10" />
+                    <TreeItem nodeId="7" label="Employee11" />
                 </TreeItem>
-                <TreeItem nodeId="4" label="Employee12" />
-                <TreeItem nodeId="4" label="Employee13" />
+                <TreeItem nodeId="5" label="Employee12" />
+                <TreeItem nodeId="5" label="Employee13" />
             </TreeItem>
         </TreeItem>
     </TreeView>
