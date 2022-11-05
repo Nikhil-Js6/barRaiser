48f075bd4b86bbd5005d54c8b84f1984f9b303ba
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-11-05 13:47:15 +0530

    Add files via upload

---
+++ src/pages/Hierarchy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tree from '../components/mui/Tree'
+
+const Hierarchy = () => {
+    return (
+        <div className='container'>
+            <Tree />
+        </div>
+    )
+}
+
+export default Hierarchy
